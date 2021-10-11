eb561c451da8e804bcf4f2961af3d1db08600d12
Cláudio Rapôso <engcfraposo@gmail.com>
2021-10-11 16:54:29 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Switch from '@mui/material/Switch';
+import { useState } from 'react';
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 function App() {
+  const [checked, setChecked] = useState<boolean>(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch {...label} 
+        onChange={
+          () => setChecked(!checked)
+        } 
+        checked={checked} 
+      />
+      <Switch {...label} 
+        onChange={
+          () => setChecked(!checked)
+        } 
+        checked={checked} 
+      />
+      <Switch {...label} 
+        onChange={
+          () => setChecked(!checked)
+        } 
+        checked={checked} 
+      />
+      <Switch {...label} 
+        onChange={
+          () => setChecked(!checked)
+        } 
+        checked={checked} 
+      />
     </div>
   );
 }
